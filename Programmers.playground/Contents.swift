0e580de937922f22ab1fcf8163989916f74efeed
Gray <eungu0920@inu.ac.kr>
2022-12-06 22:17:23 +0900

    하샤드 수

--- Programmers.playground/Contents.swift
+++ Programmers.playground/Contents.swift
@@ -1,11 +1,2 @@
 import UIKit
 import Foundation
-
-var n: Int64 = 12345
-
-var result: String = String(n)
-var arr: [Int] = []
-
-while !result.isEmpty {
-    arr.append(Int(String(result.removeLast()))!)
-}
