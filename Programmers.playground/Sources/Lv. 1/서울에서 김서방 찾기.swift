2a7866c76057a889e75ae362138adc9b6386023e
Gray <eungu0920@inu.ac.kr>
2022-12-12 13:40:02 +0900

    서울에서 김서방 찾기

---
+++ Programmers.playground/Sources/Lv. 1/서울에서 김서방 찾기.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+/*
+ String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+
+ 제한사항
+ - seoul은 길이 1 이상, 1000 이하인 배열입니다.
+ - seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
+ - "Kim"은 반드시 seoul 안에 포함되어 있습니다.
+ */
+
+public func 서울에서_김서방_찾기(_ seoul: [String]) -> String {
+    var count: Int = 0
+    
+    for search in seoul {
+        if search == "Kim" {
+            break
+        }
+        count += 1
+    }
+    
+    return "김서방은 \(count)에 있다"
+}
+
+//다른풀이
+//func solution(_ seoul: [String]) -> String { "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"}
